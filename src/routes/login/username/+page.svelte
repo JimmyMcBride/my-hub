f11a8f74ae2dc1c93a14d221a12dfe51d8fb973f
Jimmy McBride <mcbride967@gmail.com>
2023-08-27 10:58:51 -0500

    giving it a shot

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,60 +1,54 @@
 <script lang="ts">
-	import AuthCheck from "$lib/components/AuthCheck.svelte";
-	import { db, user, userData } from "$lib/firebase";
-	import { doc, getDoc, writeBatch } from "firebase/firestore";
+	import AuthCheck from "$lib/components/AuthCheck.svelte"
+	import { db, user, userData } from "$lib/firebase"
+	import { doc, getDoc, writeBatch } from "firebase/firestore"
 
-	let username = "";
-	let loading = false;
-	let isAvailable = false;
+	let username = ""
+	let loading = false
+	let isAvailable = false
 
 	// eslint-disable-next-line no-undef
-	let debounceTimer: NodeJS.Timeout;
+	let debounceTimer: NodeJS.Timeout
 
-	const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+	const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/
 
-	$: isValid = username?.length > 2 && username.length < 16 && re.test(username);
-	$: isTouched = username.length > 0;
-	$: isTaken = isValid && !isAvailable && !loading;
+	$: isValid = username?.length > 2 && username.length < 16 && re.test(username)
+	$: isTouched = username.length > 0
+	$: isTaken = isValid && !isAvailable && !loading
 
 	async function checkAvailability() {
-		isAvailable = false;
-		clearTimeout(debounceTimer);
+		isAvailable = false
+		clearTimeout(debounceTimer)
 
-		loading = true;
+		loading = true
 
 		debounceTimer = setTimeout(async () => {
-			console.log("checking availability of", username);
+			console.log("checking availability of", username)
 
-			const ref = doc(db, "usernames", username);
-			const exists = await getDoc(ref).then((doc) => doc.exists());
+			const ref = doc(db, "usernames", username)
+			const exists = await getDoc(ref).then((doc) => doc.exists())
 
-			isAvailable = !exists;
-			loading = false;
-		}, 500);
+			isAvailable = !exists
+			loading = false
+		}, 500)
 	}
 
 	async function confirmUsername() {
-		console.log("confirming username", username);
-		const batch = writeBatch(db);
-		batch.set(doc(db, "usernames", username), { uid: $user?.uid });
+		console.log("confirming username", username)
+		const batch = writeBatch(db)
+		batch.set(doc(db, "usernames", username), { uid: $user?.uid })
 		batch.set(doc(db, "users", $user!.uid), {
 			username,
 			photoURL: $user?.photoURL ?? null,
 			published: true,
-			bio: "I am the Walrus",
-			links: [
-				{
-					title: "Test Link",
-					url: "https://myhub.page",
-					icon: "custom",
-				},
-			],
-		});
+			bio: "Welcome to my hub link page!",
+			links: [],
+		})
 
-		await batch.commit();
+		await batch.commit()
 
-		username = "";
-		isAvailable = false;
+		username = ""
+		isAvailable = false
 	}
 </script>
 
